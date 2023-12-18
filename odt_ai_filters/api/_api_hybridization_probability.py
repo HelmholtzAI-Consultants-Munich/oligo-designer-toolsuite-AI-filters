99f4802cc354deb1d5169909d02fbce2f57d7c7f
Francesco Campi <francesco.campi98@gmail.com>
2023-12-18 16:48:02 +0100

    bug fixes

--- odt_ai_filters/api/_api_hybridization_probability.py
+++ odt_ai_filters/api/_api_hybridization_probability.py
@@ -6,7 +6,7 @@ import torch
 
 from ._api_base import APIBase
 from ..hybridization_probability._dataset import RNNDatasetInference ,pack_collate
-from ..hybridization_probability._models import OligoLSTM
+from ..hybridization_probability._models import OligoLSTM, OligoRNN
 
 
 
@@ -14,21 +14,19 @@ class APIHybridizationProbability(APIBase):
 
     def __init__(self, ai_filter_path) -> None:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        # get repository location
-        repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+    
         if ai_filter_path is None:
             # if none the predefined models are used
-            repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            # get repository location
+            repo_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
             self.ai_filter_path = os.path.join(repo_path,"data", "pretrained_models", "hybridization_probability.pt")
         else:
             self.ai_filter_path = ai_filter_path
-        ai_filter_path = os.path.join(repo_path,"data", "models", "hybridization_probability.pt")
-        loaded_model = torch.load(ai_filter_path, map_location=self.device)
+        loaded_model = torch.load(self.ai_filter_path, map_location=self.device)
         # load the model with the right hyperparameters
-        self.model = OligoLSTM(**loaded_model["hyperparameters"]["model"]) # model with best performances
+        self.model = OligoRNN(**loaded_model["hyperparameters"]["model"]) # model with best performances
         # load the pretrained weights of the model
-        self.model.load_state_dict(torch.load(loaded_model["weights"]))
+        self.model.load_state_dict(loaded_model["weights"])
         self.model.to(self.device)
         self.model.eval()
         # funtion to restore the predictions to the hybridization probablity
--- odt_ai_filters/hybridization_probability/__init__.py
+++ odt_ai_filters/hybridization_probability/__init__.py
@@ -1,4 +1,4 @@
-from _dataset import *
-from _models import *
+from ._dataset import *
+from ._models import *
 
 __all__ = ["MLPDataset", "RNNDataset", "RNNDatasetInference", "pack_collate", "OligoMLP", "OligoRNN", "OligoLSTM"]
