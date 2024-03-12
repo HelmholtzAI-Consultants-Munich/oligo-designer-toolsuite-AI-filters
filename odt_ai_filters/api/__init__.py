c769cd6c90a8f1b277f7f4c86f77144441713952
Francesco Campi <francesco.campi98@gmail.com>
2024-03-12 16:42:54 +0100

    dataset generation is in api

--- odt_ai_filters/api/__init__.py
+++ odt_ai_filters/api/__init__.py
@@ -1,4 +1,5 @@
 from ._api_base import APIBase
 from ._api_hybridization_probability import APIHybridizationProbability
+from ._utils import generare_api
 
-__all__ = ["APIBase", "_api_hybridization_probability"]
+__all__ = ["APIBase", "APIHybridizationProbability", "generare_api"]
--- odt_ai_filters/api/_api_hybridization_probability.py
+++ odt_ai_filters/api/_api_hybridization_probability.py
@@ -1,9 +1,12 @@
 import os
+from typing import List
 
 import pandas as pd
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+from Bio.SeqUtils import gc_fraction
+from Bio import Seq
 
 from ._api_base import APIBase
 from ..hybridization_probability._dataset import RNNDatasetInference ,pack_collate_inference
@@ -34,9 +37,11 @@ class APIHybridizationProbability(APIBase):
         self.inverse_predictions = lambda predictions: 10**(predictions*loaded_model["hyperparameters"]["dataset"]["std"] + loaded_model["hyperparameters"]["dataset"]["mean"])
     
     
-    def predict(self, data: pd.DataFrame):
+    def predict(self, queries: List[Seq.Seq], gapped_queries: List[Seq.Seq], targets: List[Seq.Seq], gapped_targets: List[Seq.Seq]):
+        # generate the dataset
+        dataset = self._generate_dataset(queries, gapped_queries, targets, gapped_targets)
         #set batch size based on the hardware available
-        batch_size = 32 if self.device == "cuda" else 1
+        batch_size = 32 if self.device == "cuda" else 1 #TODO: find optimal value
         dataset = RNNDatasetInference(data)
         dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn = pack_collate_inference)
         predictions = torch.tensor([])
@@ -47,3 +52,43 @@ class APIHybridizationProbability(APIBase):
         predictions = predictions.detach().cpu().numpy()
         predictions = self.inverse_predictions(predictions)
         return predictions
+
+    def _generate_dataset(
+            self, queries: List[Seq.Seq], gapped_queries: List[Seq.Seq], targets: List[Seq.Seq], gapped_targets: List[Seq.Seq]
+    ):
+        """Create a database with the information of the oligos that match the blast search.
+
+        :param queries: List with the sequences of the query oligos.
+        :type queries: list
+        :param gapped_queries: List with the sequences of the query oligos with gaps.
+        :type gapped_queries: list
+        :param targets: List with the sequences of the target oligos.
+        :type targets: list
+        :param gapped_targets: List with the sequences of the target oligos with gaps.
+        :type gapped_targets: list
+        :return: dataset
+        :rtype: pd.DataFrame
+        """
+
+        dataset = pd.DataFrame(
+            columns=[
+                "query_sequence",
+                "query_length",
+                "query_GC_content",
+                "off_target_sequence",
+                "off_target_length",
+                "off_target_GC_content",
+                "number_mismatches",
+            ]
+        )
+        dataset["query_sequence"] = gapped_queries
+        dataset["query_length"] = [len(query) for query in queries]
+        dataset["query_GC_content"] = [gc_fraction(query) for query in queries]
+        dataset["off_target_sequence"] = gapped_targets
+        dataset["off_target_length"] = [len(target) for target in targets]
+        dataset["off_target_GC_content"] = [gc_fraction(target) for target in targets]
+        dataset["number_mismatches"] = [
+            sum(query != target for query, target in zip(query, target))
+            for query, target in zip(gapped_queries, gapped_targets)
+        ]
+        return dataset
